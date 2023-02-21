90f1d1319c2de8459a849d7289679d3987e50edc
sushthecoda <coolsushaan@gmail.com>
2023-02-21 23:59:01 +0530

    4th scala

---
+++ scala/planet.scala
@@ -0,0 +1,18 @@
+object SpaceAge {
+  // Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+  def onEarth(age: Double): Double = age / 31557600
+
+  def onVenus(age: Double): Double = age / (31557600 * 0.61519726)
+
+  def onMercury(age: Double): Double = age / (31557600 * 0.2408467)
+
+  def onMars(age: Double): Double = age / (31557600 * 1.8808158)
+
+  def onJupiter(age: Double): Double = age / (31557600 * 11.862615)
+
+  def onSaturn(age: Double): Double = age / (31557600 * 29.447498)
+
+  def onUranus(age: Double): Double = age / (31557600 * 84.016846)
+
+  def onNeptune(age: Double): Double = age / (31557600 * 164.79132)
+}
