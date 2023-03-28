9692b2bf0d6ef785775f0eda1cd360b22f328113
sushthecoda <coolsushaan@gmail.com>
2023-03-29 00:03:58 +0530

    hashmap fixes

--- rust/macros.rs
+++ rust/macros.rs
@@ -1,11 +1,22 @@
+// Export the macro
 #[macro_export]
 macro_rules! hashmap {
+    // Handle the case with no arguments
     () => {
-        std::collections::HashMap::new()
+        ::std::collections::HashMap::new()
     };
-    ($( $key:expr => $value:expr ),* $(,)?) => {{
-        let mut map = std::collections::HashMap::new();
-        $( map.insert($key, $value); )*
-        map
-    }};
+    // Handle the case with a single comma
+    (,) => {
+        compile_error!("Expected key-value pairs separated by commas, but found only a comma.")
+    };
+    // Handle the case with one or more key-value pairs
+    ($( $key:expr => $value:expr ),* $(,)?) => {
+        {
+            let mut hm = ::std::collections::HashMap::new();
+            $(
+                hm.insert($key, $value);
+            )*
+            hm
+        }
+    };
 }
